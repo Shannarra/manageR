19b8f9eafee781f06e09250239f027eb0233a9f8
Shannarra <petarangelov15@gmail.com>
2024-02-01 17:42:58 +0200

    Added AA and bootstrap fixes:

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,15 @@
+AdminUser.create!(
+  name: 'Petar Angelov',
+  email: 'admin@example.com',
+  password: '123456',
+  access_type: 5  
+)
+
 User.create!(
   name: 'Petar Angelov',
   email: 'admin@example.com',
   password: '123456',
-  access_type: 5
+  access_type: 5  
 )
 
 100.times do
@@ -14,3 +21,4 @@ User.create!(
     access_type: %w[student teacher].sample,
   )
 end
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
